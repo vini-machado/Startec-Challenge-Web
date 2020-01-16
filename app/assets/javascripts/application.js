808e954ccf522a9966a436d1f0374db1c4730f42
vini-machado <viniciuscmachado22@gmail.com>
2020-01-16 02:40:01 +0000

    Right Navbar started

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//=require jquery
-//=require jquery_ujs
-//= require bootstrap
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
+//= require_tree
